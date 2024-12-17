a38ba3e411896e9ab9f6a7a66961399fa4897fb3
idcr <akarasev2004@yandex.ru>
2024-12-17 20:04:32 +0300

    update

--- query/model.py
+++ query/model.py
@@ -1,3 +1,7 @@
+from flask import request
+import json
+from string import Template
+
 from database.select import select_list
 from dataclasses import dataclass
 
@@ -10,14 +14,19 @@ class QueryResponse:
     status: bool
 
 
-def query_execute(db_config, user_input_data, sql_provider, action):
+def query_execute(db_config, user_input_data, sql_provider):
     message = ''
+    action = request.args.get('action')
     sql_name = f'{action}.sql'
     _sql = sql_provider.get(sql_name, user_input_data)
     result, schema = select_list(db_config, _sql)
 
     if not len(result):
-        error_message = 'По вашему запросу ничего не найдено'
+        message = 'По вашему запросу ничего не найдено'
         return QueryResponse(result=result, schema=schema, message=message, status=False)
+
+    with open('data/queries_result_text.json', encoding='utf-8') as f:
+        queries_result_text = json.load(f)
+    message = Template(queries_result_text[action]).substitute(user_input_data)
 
     return QueryResponse(result=result, schema=schema, message=message, status=True)
--- query/route.py
+++ query/route.py
@@ -1,7 +1,6 @@
 import os
 from flask import render_template, Blueprint, current_app, request, flash, redirect
-import json
-from string import Template
+
 
 from database.sql_provider import SQLProvider
 from access import group_required
@@ -29,16 +28,11 @@ def query_handle():
 @blueprint_query.route('/', methods=['POST'])
 #@group_required
 def query_index():
-    action = request.args.get('action')
     user_input_data = request.form.to_dict()
-    query_result = query_execute(current_app.config['db_config'], user_input_data, provider, action)
+    query_result = query_execute(current_app.config['db_config'], user_input_data, provider)
 
     if not query_result.status:
         flash(query_result.message, 'danger')
         return redirect(request.url)
 
-    with open('data/queries_result_text.json', encoding='utf-8') as f:
-        queries_result_text = json.load(f)
-    query_info = Template(queries_result_text[action]).substitute(user_input_data)
-
-    return render_template('dynamic.html', query_info=query_info, columns=query_result.schema, data=query_result.result)
+    return render_template('dynamic.html', query_info=query_result.message, columns=query_result.schema, data=query_result.result)
